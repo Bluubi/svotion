74d9ffa0d0d87e6f6b9b28fd7bd813a53e51f572
BluubiDev <bluubi.dev@gmail.com>
2023-05-27 17:18:34 +0200

    feat: create sidenav and list elements

---
+++ src/shared/List/List.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">  
+   export let list: string[];
+</script>
+
+        <ul>
+            {#each list as  item }
+                <li> { item } </li>
+            {/each}
+        </ul>
+
+<style>
+
+    ul {
+        list-style: none;
+        margin-top: 16px;
+        flex: 1 0 100%;
+    }
+
+    li {
+        padding: 0.6rem;
+    }
+
+</style>
+
---
+++ src/shared/Sidebar/Sidebar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { resizeCol } from "../../utils/resize";
+    import NewPage from "./Actions/New-page/New-page.svelte";
+    
+    
+</script>
+
+
+<nav use:resizeCol>
+    <NewPage on:action />
+</nav>
+
+
+<style>
+    nav {
+        display: flex;
+        width: 13rem;
+        min-width: 10rem;
+        height: 100vh;
+        background-color: var(--primary-bg-color);
+        flex-direction: column;
+    }
+
+    nav:is(.resizing) {
+        border-right: 6px solid var(--primary-highlight-bg-color);
+    }
+</style>
