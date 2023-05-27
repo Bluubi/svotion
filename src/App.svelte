894cfe0332244a6d8602c8a353763edb5e35439b
BluubiDev <bluubi.dev@gmail.com>
2023-05-27 17:19:18 +0200

    feat: added all features

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Header from "./shared/Header/Header.svelte";
+    import Sidebar from "./shared/Sidebar/Sidebar.svelte";
+let component
+</script>
+
+<main>
+  <Header />
+  <Sidebar on:action={(event) => component = event.detail}/>
+  <svelte:component this={component} on:destroy={(event) => component = event.detail}/>
+</main>
