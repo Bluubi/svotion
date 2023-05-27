f1d7f8eae9608d4ecd5c9fbe7e1895e0ca7fcc5d
BluubiDev <bluubi.dev@gmail.com>
2023-05-27 17:18:42 +0200

    feat: create form to add new page

---
+++ src/features/Form-new-page/Form-new-page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import List from '../../shared/List/List.svelte';
+
+   import { createEventDispatcher } from "svelte";
+   
+   const dispatch = createEventDispatcher();
+
+   const destroyItself = () => {
+        dispatch('destroy', false);
+   }
+
+   const list = ['Página vacía']
+</script>
+
+
+<section>
+     <div class="modal"> 
+        <button class="button button--x" on:click={destroyItself}> x </button>
+        <List list={list}></List>
+
+    </div>
+</section>
+
+
+<style>
+
+    section {
+        position: absolute;
+        inset: 0;
+        background: rgb(0,0,0,35%);
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    div {
+        background: white;
+        padding: 2rem;
+    }
+
+    .modal {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .button--x {
+        margin-left: auto;
+    }
+
+</style>
